65fe58923818bc8a8a4541ee5e68906ab2b1a66f
Laurent Fintoni <laurent@spinscience.org.uk>
2024-12-03 18:33:27 +0100

    update

---
+++ vocabulary_load.sh
@@ -0,0 +1,32 @@
+# load KNOT vocabs 
+
+curl -L -o ktx.ttl https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/1.2/ktx.ttl
+curl -I -X POST -H Content-Type:text/turtle -T ktx.ttl -G http://localhost:9030/skosmos/data --data-urlencode graph=https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/1.2/ktx.ttl
+
+curl -L -o kth.ttl https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/1.2/kth.ttl
+curl -I -X POST -H Content-Type:text/turtle -T kth.ttl -G http://localhost:9030/skosmos/data --data-urlencode graph=https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/1.2/kth.ttl
+
+# load Aldo Moro vocabs 
+
+curl -L -o mtv.ttl https://raw.githubusercontent.com/sbrzt/moro/refs/heads/main/voc/types.ttl
+curl -I -X POST -H Content-Type:text/turtle -T mtv.ttl -G http://localhost:9030/skosmos/data --data-urlencode graph=https://raw.githubusercontent.com/sbrzt/moro/refs/heads/main/voc/types.ttl
+
+curl -L -o mrv.ttl https://raw.githubusercontent.com/sbrzt/moro/refs/heads/main/voc/roles.ttl
+curl -I -X POST -H Content-Type:text/turtle -T mrv.ttl -G http://localhost:9030/skosmos/data --data-urlencode graph=https://raw.githubusercontent.com/sbrzt/moro/refs/heads/main/voc/roles.ttl
+
+curl -L -o msv.ttl https://raw.githubusercontent.com/sbrzt/moro/refs/heads/main/voc/subjects.ttl
+curl -I -X POST -H Content-Type:text/turtle -T msv.ttl -G http://localhost:9030/skosmos/data --data-urlencode graph=https://raw.githubusercontent.com/sbrzt/moro/refs/heads/main/voc/subjects.ttl
+
+# load ontologies
+
+curl -L -o fentry_skos.ttl https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/fentry_skos.ttl
+curl -I -X POST -H Content-Type:text/turtle -T fentry_skos.ttl -G http://localhost:9030/skosmos/data --data-urlencode graph=https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/fentry_skos.ttl
+
+curl -L -o odi-skos.ttl https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/odi-skos.ttl
+curl -I -X POST -H Content-Type:text/turtle -T odi-skos.ttl -G http://localhost:9030/skosmos/data --data-urlencode graph=https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/odi-skos.ttl
+
+curl -L -o hico-skos.ttl https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/hico-skos.ttl
+curl -I -X POST -H Content-Type:text/turtle -T hico-skos.ttl -G http://localhost:9030/skosmos/data --data-urlencode graph=https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/hico-skos.ttl
+
+curl -L -o write-skos.ttl https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/write-skos.ttl
+curl -I -X POST -H Content-Type:text/turtle -T write-skos.ttl -G http://localhost:9030/skosmos/data --data-urlencode graph=https://raw.githubusercontent.com/icdp-digital-library/KNOT/refs/heads/main/data_model/controlled_vocabularies/write-skos.ttl
